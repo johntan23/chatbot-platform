e0416c872fb421e9766a9606bb68371ea30065a9
Giannis Tanidis <149205063+johntan23@users.noreply.github.com>
2026-03-22 20:44:18 +0200

    feat: add DTOs - ChatRequest, ChatResponse

--- src/main/java/com/tanidis/chatbot/dto/ChatResponse.java
+++ src/main/java/com/tanidis/chatbot/dto/ChatResponse.java
@@ -1,4 +1,11 @@
 package com.tanidis.chatbot.dto;
 
+import lombok.Data;
+
+@Data
 public class ChatResponse {
+
+    private Long conversationId;
+    private String message;
+    private String role;
 }
